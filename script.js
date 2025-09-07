2156f3cb4ad1fc1970b9a7221ca540dc251a4952
Wero <wero@bluecollarlabs.io>
2025-09-06 21:29:46 -0700

    Alt way to structure program
    Do I want to have 1 object hold everything as a
    module or do I want various objects for different
    pieces

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const game = (function() {
-    
+    let gameBoard = ["", "", "", "", "", "", "", "", ""];
+    let running = false;
 })();
+
+// Alt way to build out different sections
+
+const gameBoard = {
+    board: ["", "", "", "", "", "", "", "", ""],
+}
+
+const players = {
+    player1: "X",
+    player2: "O",
+}
+
+const gameLogic = {
+    checkWinner: function() {}
+}
