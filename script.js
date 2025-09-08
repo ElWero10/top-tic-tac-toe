f1d5a872732789d6d1236df416ec092f50b233be
Wero <wero@bluecollarlabs.io>
2025-09-07 21:15:49 -0700

    Add variables to hold gameBoard, player, winConditions

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 const game = (function() {
     let gameBoard = ["", "", "", "", "", "", "", "", ""];
     let running = false;
+    let currentPlayer = "X";
+    let winCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    
 })();
 
 // Alt way to build out different sections
 
-const gameBoard = {
-    board: ["", "", "", "", "", "", "", "", ""],
-}
+// const gameBoard = {
+//     board: ["", "", "", "", "", "", "", "", ""],
+// }
 
-const players = {
-    player1: "X",
-    player2: "O",
-}
+// const players = {
+//     player1: "X",
+//     player2: "O",
+// }
 
-const gameLogic = {
-    checkWinner: function() {}
-}
+// const gameLogic = {
+//     checkWinner: function() {}
+// }
